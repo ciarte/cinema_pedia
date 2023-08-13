7177c3a8c4a5ceb2085543d014d1a185b338d645
Gabriel Iciarte <gabriel_viki@hotmail.com>
2023-08-13 00:02:59 -0300

    Dio config

---
+++ lib/infrastructure/datasource/moviedb_datasource.dart
@@ -0,0 +1,28 @@
+import 'package:cinema_pedia/config/constans/environment.dart';
+import 'package:cinema_pedia/domain/datasources/movies_datasource.dart';
+import 'package:cinema_pedia/domain/entities/movie.dart';
+import 'package:cinema_pedia/infrastructure/mappers/movie_mappers.dart';
+import 'package:cinema_pedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+
+class MovieDbDatasource extends MovieDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Environment.movieDbKey,
+        'language': 'es-MX'
+      }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDBResponse.results
+        .where((movieDb) => movieDb.posterPath != 'No - Image')
+        .map((movieDB) => MovieMappers.movieDBToEntity(movieDB))
+        .toList();
+
+    return movies;
+  }
+}
---
+++ lib/infrastructure/mappers/movie_mappers.dart
@@ -0,0 +1,25 @@
+import 'package:cinema_pedia/domain/entities/movie.dart';
+import 'package:cinema_pedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMappers {
+  static Movie movieDBToEntity(MovieMovieDB movieDB) => Movie(
+      adult: movieDB.adult,
+      backdropPath: (movieDB.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}'
+          : 'https://upload.wikimedia.org/wikipedia/commons/6/64/Poster_not_available.jpg',
+      genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: (movieDB.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500/${movieDB.posterPath}'
+          // : 'https://upload.wikimedia.org/wikipedia/commons/6/64/Poster_not_available.jpg',
+          : 'No - Image',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount);
+}
